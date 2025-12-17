ce09f3a87cdc08592059616a4e2099258e537564
Scarounine <paulinelesueur93@gmail.com>
2025-12-17 12:24:49 +0100

    feat: implement auth/me route

---
+++ backend/src/main/java/com/openclassrooms/chatop/DTO/UserResponse.java
@@ -0,0 +1,51 @@
+package com.openclassrooms.chatop.DTO;
+
+import java.time.LocalDate;
+
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
