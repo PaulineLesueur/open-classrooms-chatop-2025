85715c87970048f7ec92ae89d872ca58a80a8405
Scarounine <paulinelesueur93@gmail.com>
2025-12-19 19:25:27 +0100

    feat: add sql script to database creation

--- frontend/ressources/sql/script.sql
+++ frontend/ressources/sql/script.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE chatop;
+USE chatop;
+
 CREATE TABLE `USERS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255),
@@ -35,3 +38,12 @@ ALTER TABLE `RENTALS` ADD FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`);
 ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
 
 ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`);
+
+INSERT INTO USERS (email, name, password, created_at, updated_at) VALUES
+('test@test.com', 'John DOE', '$2a$10$ThFlk/qkG3iZZwpAfcJy0eNSRivxpF9TDxjvoOH3dDRNBXhWPSryy', NOW(), NOW());
+
+INSERT INTO RENTALS (name, surface, price, picture, description, owner_id, created_at, updated_at) VALUES
+('Modern family house', 150, 350, 'modern_family_house.jpg', 'A beautiful and spacious family house in the suburbs.', 1, NOW(), NOW()),
+('Charming cottage', 70, 100, 'charming_cottage.jpg', 'Charming little cottage nestled in the countryside.', 1, NOW(), NOW()),
+('Mediterranean_Villa', 250, 830, 'mediterranean_villa.jpg', 'Modern and spacious mediterranean villa by the sea.', 1, NOW(), NOW());
+
